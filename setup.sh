006cf4dfccdc57d0b5d77b543c5c8d9b68c70ebb
Logan Bunch <loganbunch@gmail.com>
2023-10-01 20:03:19 +0000

    Changes to install scripts, new zshrc

--- setup.sh
+++ setup.sh
@@ -2,9 +2,26 @@
 
 sudo pacman -S archlinux-keyring
 
-sudo pacman -S yay
+sudo pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+makepkg -si
 
-yay blueberry blueman bluez brave-bin brightnessctl btop bunjs calc eza fd ffmpeg gdm gimp github-cli go graphite grim hyprland-git kitty kitty-shell-integration lazygit logseq-bin mako neovim nodejs npm polkit polkit-kde polkit-qt5 ripgrep swayidle thunar waylock wl-gammarelay-rs wofi workstyle-git wpaperd xclip xdg-desktop-portal-hyprland-git zsh oh-my-zsh-git waybar-hyprland-git
+cd ~
+
+rust= yay -S rustup
+
+$rust
+
+rustup= rustup default stable
+
+$rustup
+
+install= yay -S blueberry blueman bluez brave-bin brightnessctl btop bunjs calc eza fd ffmpeg gdm gimp github-cli go graphite grim hyprland-git kitty kitty-shell-integration lazygit logseq-bin mako neovim nodejs npm polkit polkit-kde polkit-qt5 ripgrep swayidle thunar waylock wl-gammarelay-rs wofi workstyle-git wpaperd xclip xdg-desktop-portal-hyprland-git zsh waybar-hyprland-git gsettings-qt nwg-look-bin
+
+$install
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
 
@@ -14,6 +31,8 @@ cp .zshrc ~
 
 cp -r .config ~
 
-chsh -s $(which zsh)
+sudo systemctl enable gdm.service
+
+sudo systemctl enable bluetooth.service && sudo systemctl start bluetooth.service
 
 echo "Setup complete, please reboot"
